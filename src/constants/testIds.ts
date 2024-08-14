85cb058486eb40421ba91d55bf98cd5df4b0c358
ammarr <ahmedmahmoud9597@gmail.com>
2024-08-14 20:38:14 +0400

    test: add test ids

---
+++ src/constants/testIds.ts
@@ -0,0 +1,17 @@
+export const testIds = {
+  splash: {
+    title: 'splash-title',
+  },
+  auth: {
+    signUpTitle: 'signup',
+    emailInput: 'email-input',
+    emailError: 'email-error',
+    usernameInput: 'username-input',
+    usernmaeError: 'username-error',
+    passwordInput: 'password-input',
+    passwordError: 'password-error',
+    loginButton: 'login-button',
+    settingsButton: 'settings-button',
+    signupButton: 'signup-button',
+  },
+};
